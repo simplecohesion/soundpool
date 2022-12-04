0f62153e7dea8433cc09cfa2f2e3cb843b5d217a
Roscoe <roscoe@simplecohesion.com>
2022-12-05 09:22:44 +1100

    starling (#2)

--- soundpool/ios/Classes/SwiftSoundpoolPlugin.swift
+++ soundpool/ios/Classes/SwiftSoundpoolPlugin.swift
@@ -1,7 +1,7 @@
+import Foundation
 import Flutter
 import UIKit
 import AVFoundation
-
 
 public class SwiftSoundpoolPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -22,10 +22,7 @@ public class SwiftSoundpoolPlugin: NSObject, FlutterPlugin {
             
             initAudioSession(attributes)
             
-            let maxStreams = attributes["maxStreams"] as! Int
-            let enableRate = (attributes["ios_enableRate"] as? Bool) ?? false
-            let wrapper = SoundpoolWrapper(maxStreams, enableRate)
-            
+            let wrapper = SoundpoolWrapper()
             let index = counter.increment()
             wrappers[index] = wrapper;
             result(index)
@@ -123,233 +120,113 @@ public class SwiftSoundpoolPlugin: NSObject, FlutterPlugin {
         return wrapper
     }
     
-    class SoundpoolWrapper : NSObject {
-        private var maxStreams: Int
-        
-        private var enableRate: Bool
-        
-        private var streamIdProvider = Atomic<Int>(0)
-        
-        private lazy var soundpool = [AVAudioPlayer]()
-        
-        private lazy var streamsCount: Dictionary<Int, Int> = [Int: Int]()
-        
-        private lazy var nowPlaying: Dictionary<Int, NowPlaying> = [Int: NowPlaying]()
-        
-        init(_ maxStreams: Int, _ enableRate: Bool){
-            self.maxStreams = maxStreams
-            self.enableRate = enableRate
-        }
-        
+    class SoundpoolWrapper : NSObject {    
+
+        private var starling: Starling = Starling()
+
         public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
             let attributes = call.arguments as! NSDictionary
-            //            print("\(call.method): \(attributes)")
             switch call.method {
             case "load":
-                let rawSound = attributes["rawSound"] as! FlutterStandardTypedData
-                do {
-                    let audioPlayer = try AVAudioPlayer(data: rawSound.data)
-                    if (enableRate){
-                        audioPlayer.enableRate = true
-                    }
-                    audioPlayer.prepareToPlay()
-                    let index = soundpool.count
-                    soundpool.append(audioPlayer)
-                    result(index)
-                } catch {
-                    result(-1)
-                }
+                result(-1)
             case "loadUri":
                 let soundUri = attributes["uri"] as! String
                 
-                let url = URL(string: soundUri)
-                if (url != nil){
-                    DispatchQueue.global(qos: .utility).async {
-                        do {
-                            let cachedSound = try Data(contentsOf: url!, options: NSData.ReadingOptions.mappedIfSafe)
-                            DispatchQueue.main.async {
-                                var value:Int = -1
-                                do {
-                                    let audioPlayer = try AVAudioPlayer(data: cachedSound)
-                                    if (self.enableRate){
-                                        audioPlayer.enableRate = true
-                                    }
-                                    audioPlayer.prepareToPlay()
-                                    let index = self.self.soundpool.count
-                                    self.self.soundpool.append(audioPlayer)
-                                    value = index
-                                } catch {
-                                    print("Unexpected error while preparing player: \(error).")
-                                }
-                                result(value)
-                            }
-                        } catch {
-                            print("Unexpected error while downloading file: \(error).")
-                            DispatchQueue.main.async {
-                                result(-1)
-                            }
-                        }
-                    }
-                } else {
+                guard let url = URL(string: soundUri) else {
                     result(-1)
+                    return
                 }
+
+                let res = starling.load(sound: url)
+                result(res)
+
             case "play":
-                let soundId = attributes["soundId"] as! Int
-                let times = attributes["repeat"] as? Int
-                let rate = (attributes["rate"] as? Double) ?? 1.0
-                if (soundId < 0){
+                let streamId = attributes["soundId"] as! Int
+                if (streamId < 0){
                     result(0)
                     return
                 }
-                
-                guard var audioPlayer = playerBySoundId(soundId: soundId) else {
-                    result(0)
-                    return
-                }
-                DispatchQueue.global(qos: .userInitiated).async {
-                    do {
-                        let currentCount = self.streamsCount[soundId] ?? 0
-                        
-                        if (currentCount >= self.maxStreams){
-                            result(0)
-                            return
-                        }
-                        
-                        let nowPlayingData: NowPlaying
-                        let streamId: Int = self.streamIdProvider.increment()
-                        
-                        let delegate = SoundpoolDelegate(pool: self, soundId: soundId, streamId: streamId)
-                        audioPlayer.delegate = delegate
-                        nowPlayingData =  NowPlaying(player: audioPlayer, delegate: delegate)
-                        
-                        // audioPlayer.numberOfLoops = times ?? 0
-                        // if (self.enableRate){
-                        //     audioPlayer.enableRate = true
-                        //     audioPlayer.rate = Float(rate)
-                        // }
-                        
-                        if (audioPlayer.play()) {
-                            self.streamsCount[soundId] = currentCount + 1
-                            self.nowPlaying[streamId] = nowPlayingData
-                            result(streamId)
-                        } else {
-                            result(0) // failed to play sound
-                        }
-                        // lets recreate the audioPlayer for next request - setting numberOfLoops has initially no effect
-                        
-                        // if let previousData = audioPlayer.data {
-                        //     audioPlayer = try AVAudioPlayer(data: previousData)
-                        // } else if let previousUrl = audioPlayer.url {
-                        //     audioPlayer = try AVAudioPlayer(contentsOf: previousUrl)
-                        // }
-                        // if (self.enableRate){
-                        //     audioPlayer.enableRate = true
-                        // }
-                        // audioPlayer.prepareToPlay()
-                        // self.soundpool[soundId] = audioPlayer
-                    } catch {
-                        result(0)
-                    }
-                }
+                starling.play(streamId)
+                result(streamId)
             case "pause":
                 let streamId = attributes["streamId"] as! Int
-                if let playingData = playerByStreamId(streamId: streamId) {
-                    playingData.player.pause()
-                    result(streamId)
-                } else {
-                    result (-1)
-                }
+                starling.stop(streamId)
+                result(streamId)
             case "resume":
                 let streamId = attributes["streamId"] as! Int
-                if let playingData = playerByStreamId(streamId: streamId) {
-                    playingData.player.play()
-                    result(streamId)
-                } else {
-                    result (-1)
-                }
+                starling.play(streamId)
+                result(streamId)
             case "stop":
                 let streamId = attributes["streamId"] as! Int
-                if let nowPlaying = playerByStreamId(streamId: streamId) {
-                    let audioPlayer = nowPlaying.player
-                    audioPlayer.stop()
-                    result(streamId)
-                    // removing player
-                    self.nowPlaying.removeValue(forKey: streamId)
-                    nowPlaying.delegate.decreaseCounter()
-                    audioPlayer.delegate = nil
-                } else {
-                    result(-1)
-                }
+                starling.stop(streamId)
+                result(streamId)
             case "setVolume":
-                let streamId = attributes["streamId"] as? Int
-                let soundId = attributes["soundId"] as? Int
-                let volume = attributes["volumeLeft"] as! Double
-                
-                var audioPlayer: AVAudioPlayer? = nil;
-                if (streamId != nil){
-                    audioPlayer = playerByStreamId(streamId: streamId!)?.player
-                } else if (soundId != nil){
-                    audioPlayer = playerBySoundId(soundId: soundId!)
-                }
-                audioPlayer?.volume = Float(volume)
-                result(nil)
+                result(-1)
             case "setRate":
-                if (enableRate){
-                    let streamId = attributes["streamId"] as! Int
-                    let rate = (attributes["rate"] as? Double) ?? 1.0
-                    let audioPlayer: AVAudioPlayer? = playerByStreamId(streamId: streamId)?.player
-                    audioPlayer?.rate = Float(rate)
-                }
-                result(nil)
+                result(-1)
             case "release": // TODO this should distinguish between soundpools for different types of audio playbacks
-                stopAllStreams()
-                soundpool.removeAll()
+                dispose()
                 result(nil)
             default:
                 result("notImplemented")
             }
         }
+
+        func dispose() {
+            starling.dispose()
+        }
         
         func stopAllStreams() {
-            for audioPlayer in soundpool {
-                audioPlayer.stop()
-            }
+            starling.stopAll()
         }
-        private func playerByStreamId(streamId: Int) -> NowPlaying? {
-            let audioPlayer = nowPlaying[streamId]
-            return audioPlayer
-        }
-        
-        private func playerBySoundId(soundId: Int) -> AVAudioPlayer? {
-            if (soundId >= soundpool.count || soundId < 0){
-                return nil
-            }
-            let audioPlayer = soundpool[soundId]
-            return audioPlayer
-        }
-        
-        private class SoundpoolDelegate: NSObject, AVAudioPlayerDelegate {
-            private var soundId: Int
-            private var streamId: Int
-            private var pool: SoundpoolWrapper
-            init(pool: SoundpoolWrapper, soundId: Int, streamId: Int) {
-                self.soundId = soundId
-                self.pool = pool
-                self.streamId = streamId
-            }
-            func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-                decreaseCounter()
-            }
-            func decreaseCounter(){
-                pool.streamsCount[soundId] = (pool.streamsCount[soundId] ?? 1) - 1
-                pool.nowPlaying.removeValue(forKey: streamId)
-            }
-        }
-        
-        private struct NowPlaying {
-            let player: AVAudioPlayer
-            let delegate: SoundpoolDelegate
-        }
+
     }
 }
 
+final class Atomic<T> {
+
+    private let sema = DispatchSemaphore(value: 1)
+    private var _value: T
+
+    init (_ value: T) {
+        _value = value
+    }
+
+    var value: T {
+        get {
+            sema.wait()
+            defer {
+                sema.signal()
+            }
+            return _value
+        }
+        set {
+            sema.wait()
+            _value = newValue
+            sema.signal()
+        }
+    }
+
+    func swap(_ value: T) -> T {
+        sema.wait()
+        let v = _value
+        _value = value
+        sema.signal()
+        return v
+    }
+}
+
+extension Atomic where T == Int {
+    
+    func increment() -> Int {
+        return increment(n: 1)
+    }
+
+    func increment(n: Int) -> Int {
+        sema.wait()
+        let v = _value + n
+        _value = v
+        sema.signal()
+        return v
+    }
+}
--- soundpool/ios/soundpool.podspec
+++ soundpool/ios/soundpool.podspec
@@ -3,7 +3,7 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'soundpool'
-  s.version          = '0.0.1'
+  s.version          = '0.0.2'
   s.summary          = 'A Flutter sound pool for playing short media files'
   s.description      = <<-DESC
 A Flutter sound pool for playing short media files
